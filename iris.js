0a590e4a7fecf3aadc8db5818847eb91a7865377
Vishal Jain <vjtinu.jain431@gmail.com>
2017-03-25 10:58:54 +0530

    Scatter plot iris dataset

--- iris.js
+++ iris.js
@@ -1,6 +1,4 @@
-
 //scatter plot of Iris dataset
-
 //pluggable variables 
 	var outerWidth = 300,
 	    innerHeight = 250,
@@ -34,15 +32,12 @@
 	  xScale.domain(d3.extent(data,function(d){return d[xColumn];}));
 	  yScale.domain(d3.extent(data,function(d){return d[yColumn];}));
 	  rScale.domain(d3.extent(data,function(d){return d[rColumn];}));
-
-
 	  //bind
 	  var circles = svg.selectAll('circle').data(data);
 	  //enter
 	  circles.enter().append('circle').attr('r',function(d){return rScale(d[rColumn]);})
 	  //update
 	  .merge(circles)
-
 	    .attr("cx", function (d){ return xScale(d[xColumn]); })
 	    .attr("cy", function (d){ return yScale(d[yColumn]); })
 	    .attr("fill",    function (d){ return   colorScale(d[colorColumn]); });
@@ -50,4 +45,3 @@
 	  circles.exit().remove();
 	}
 	d3.csv('iris.csv',type,render);
-
